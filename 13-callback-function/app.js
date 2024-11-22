530e4217e64fed555f22a16ad753735e6634721f
Safwen-Saafi <saafi.saf0@gmail.com>
2024-11-22 23:44:51 +0100

    Array.from()

---
+++ 13-callback-function/app.js
@@ -0,0 +1,17 @@
+function makeUppercase(value) {
+  console.log(value.toUpperCase())
+}
+// makeUppercase('peter')
+
+function reverseString(value) {
+  console.log(value.split('').reverse().join(''))
+}
+
+function handleName(name, cb) {
+  const fullName = `${name} smith`
+  console.log(fullName)
+  cb(fullName)
+}
+
+// handleName('peter', makeUppercase)
+// handleName('peter', reverseString)
